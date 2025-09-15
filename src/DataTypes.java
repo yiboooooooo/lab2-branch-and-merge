87c5c7a7bca9e181369c9c420adb9d778708e675
yiboooooooo <152505267+yiboooooooo@users.noreply.github.com>
2025-09-15 15:52:23 -0400

    remove todo

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,5 +1,4 @@
 public class DataTypes {
-    // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
     public static long sum(int[] numbers) {
         long s = 0; // variable to accumulate the sum in!
         // below is a "foreach" loop which iterates through numbers
